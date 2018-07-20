52130f66575acf92979ad10b21ae75f88713fb3a
Christopher Parks <cparks@TDY.Teledyne.com>
2018-07-20 09:14:03 -0400

    Fixed method for removing duplicates. It was not returning the head position. Added more test cases.

--- linked_list_questions.py
+++ linked_list_questions.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 class LinkedListQuestions:
     @staticmethod
-    def remove_duplicates(linked_list:Node):
+    def remove_duplicates(head:Node):
         """
         Question 2.1
         Write code to remove duplicates from an unsorted linked list.
@@ -12,16 +12,18 @@ class LinkedListQuestions:
         """
         val_count = defaultdict(int)  # Dictionary to keep track of the count of how many times a value has appeared.
 
-        while linked_list.next is not None:
-            val_count[linked_list.data] += 1
+        head_ref_save = head  # Save the head position so we may return it after operating
 
-            while val_count[linked_list.next.data] > 1:  # Begin searching for an element that is not a duplicate
-                val_count[linked_list.next.data] += 1
-                linked_list.next = linked_list.next.next
+        while head is not None and head.next is not None:
+            val_count[head.data] += 1
 
-            linked_list = linked_list.next  # Move to the next position
+            while head.next is not None and val_count[head.next.data] >= 1:  # Begin searching for an element that is not a duplicate
+                val_count[head.next.data] += 1
+                head.next = head.next.next
 
-        return linked_list
+            head = head.next  # Move to the next position
+
+        return head_ref_save
 
     @staticmethod
     def kth_to_last():
--- test_linkedListQuestions.py
+++ test_linkedListQuestions.py
@@ -2,14 +2,17 @@ from unittest import TestCase
 from linked_list_questions import LinkedListQuestions
 from LinkedList import Node
 
+
 class TestLinkedListQuestions(TestCase):
     def test_remove_duplicates(self):
-        test_list = [1,2,3,4,4,4,5,5,6]
-        list_no_dups = Node.toList(LinkedListQuestions.remove_duplicates(Node.fromList(test_list)))
+        test_cases = [([1, 2, 3, 4, 4, 4, 5, 5, 5, 5, 6], [1, 2, 3, 4, 5, 6]),
+                      ([1, 2, 3, 4, 5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
+                      ([1, 1, 2, 1, 3, 4, 4, 3, 2, 5, 3, 3, 5, 6, 6, 6], [1, 2, 3, 4, 5, 6]),
+                      ([1, 1, 1, 1], [1]),
+                      ([], [None])]
 
-        print(list_no_dups)
-
-        pass#self.fail()
+        for test_list, result in test_cases:
+            self.assertListEqual(Node.toList(LinkedListQuestions.remove_duplicates(Node.fromList(test_list))), result)
 
     def test_kth_to_last(self):
         pass#self.fail()
